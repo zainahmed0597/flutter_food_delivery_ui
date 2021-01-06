80bafb702424e705044a59706c0a3feea1b3a88f
zainahmed0597 <zainahmed0597@gmail.com>
2021-01-07 01:55:36 +0500

    working main UI

---
+++ lib/widgets/rating_stars.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RatingStars extends StatelessWidget {
+
+  final int rating;
+
+  RatingStars(this.rating);
+
+  @override
+  Widget build(BuildContext context) {
+    String stars = '';
+
+    return Text('stars');
+
+  }
+}
